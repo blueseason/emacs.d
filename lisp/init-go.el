1fed83ba38b2a7a6e5fdd79e2475f66ab54bb2a2
season <blueseason@gmail.com>
2017-04-14 09:38:35 +0800

    update go-guru insteadof oracle update js ident to 4

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,9 +1,10 @@
 (require-package 'go-mode)
 (require-package 'company-go)
+(require-package 'go-guru)
 
-(setenv "GOPATH" "/home/season/goworkspace")
+(setenv "GOPATH" "/home/season/gowork")
 
-(setenv "PATH" "/home/season/.rvm/gems/ruby-2.0.0-p451/bin:/home/season/.rvm/gems/ruby-2.0.0-p451@global/bin:/home/season/.rvm/rubies/ruby-2.0.0-p451/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/season/go/bin:/home/season/.rvm/bin:/home/season/workspace/Ant/bin:$GOPATH/bin")
+(setenv "PATH" "/home/season/.rvm/gems/ruby-2.1.5/bin:/home/season/.rvm/gems/ruby-2.1.5@global/bin:/home/season/.rvm/rubies/ruby-2.1.5/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/home/season/go/bin:/home/season/.rvm/bin$GOPATH/bin")
 (require 'go-mode-autoloads)
 
 (add-hook 'before-save-hook 'gofmt-before-save)
@@ -17,11 +18,12 @@
 (add-hook 'go-mode-hook '(lambda ()
                            (local-set-key (kbd "C-c C-k") 'godoc)))
 
-(load "$GOPATH/src/code.google.com/p/go.tools/cmd/oracle/oracle.el")
-(add-hook 'go-mode-hook 'go-oracle-mode)
 
-(add-to-list 'load-path "~/goworkspace/src/github.com/dougm/goflymake")
-;(load "$GOPATH/src/github.com/dougm/goflymake/go-flymake.el")
+(go-guru-hl-identifier-mode)
+(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+
+(add-to-list 'load-path "$GOPATH/src/github.com/dougm/goflymake")
+(load "$GOPATH/src/github.com/dougm/goflymake/go-flymake.el")
 (require 'go-flymake)
 
 ;; (load "$GOPATH/src/github.com/nsf/gocode/emacs/go-autocomplete.el")
