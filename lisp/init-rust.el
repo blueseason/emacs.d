a29f22fbd1b8ae9ffe3774b9fc2b4f299a30cf35
blueseason <blueseason@gmail.com>
2021-06-22 17:54:11 +0800

    update rust and lsp

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -3,14 +3,56 @@
 ;;; Code:
 
 (when (maybe-require-package 'rust-mode)
-  (when (maybe-require-package 'racer)
-    (add-hook 'rust-mode-hook #'racer-mode))
+  ;; (when (maybe-require-package 'racer)
+  ;;   (add-hook 'rust-mode-hook #'racer-mode))
   (when (maybe-require-package 'company)
     (add-hook 'racer-mode-hook #'company-mode)))
 
 (when (maybe-require-package 'flycheck-rust)
   (with-eval-after-load 'rust-mode
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
+
+(use-package lsp-mode
+  ;; 延时加载：仅当 (lsp) 函数被调用时再 (require)
+  :commands (lsp lsp-deferred)
+  ;; 在哪些语言 major mode 下启用 LSP
+  :hook (((rust-mode
+           ;; ......
+           ) . lsp-deferred))
+  :init ;; 在 (reuqire) 之前执行
+
+  :custom
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-cargo-watch-enable nil)
+
+  )
+
+;; 内容呈现
+(use-package lsp-ui
+  ;; 仅在某软件包被加载后再加载
+  :after (lsp-mode)
+  ;; 延时加载
+  :commands (lsp-ui-mode)
+  :bind
+  (:map lsp-ui-mode-map
+        ;; 查询符号定义：使用 LSP 来查询。通常是 M-.
+        ([remap xref-find-references] . lsp-ui-peek-find-references)
+        ;; 查询符号引用：使用 LSP 来查询。通常是 M-?
+        ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+        ;; 该文件里的符号列表：类、方法、变量等。前提是语言服务支持本功能。
+        ("C-c u" . lsp-ui-imenu))
+  ;; 当 lsp 被激活时自动激活 lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :init
+  ;; lsp-ui 有相当细致的功能开关。具体参考：
+  ;; https://github.com/emacs-lsp/lsp-mode/blob/master/docs/tutorials/how-to-turn-off.md
+  (setq lsp-enable-symbol-highlighting nil
+        lsp-ui-doc-enable nil
+        lsp-lens-enable t))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
